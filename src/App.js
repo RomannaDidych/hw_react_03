3079cb7c73e97a9b0e3cbb592114815e93f5c481
unknown <romanna.d@gmail.com>
2021-01-15 16:18:37 +0200

    fixed tick

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
-import './App.css';
-import Timer from './Components/Timer';
+import "./App.css";
+import Timer from "./Components/Timer";
 
 function App() {
   return (
     <div className="App">
-      <Timer timer={{
-              time: 5000,
-              timeTick: 500,
-              autostart: false
-            }} />
-      <Timer timer={{
-              time: 10000,
-              timeTick: 1000,
-              autostart: true
-            }} />         
+      <Timer
+        timer={{
+          time: 5000,
+          timeTick: 535,
+          autostart: false,
+        }}
+      />
+      <Timer
+        timer={{
+          time: 10000,
+          timeTick: 1000,
+          autostart: true,
+        }}
+      />
     </div>
   );
 }
--- src/Components/Timer.js
+++ src/Components/Timer.js
@@ -1,76 +1,78 @@
-import React, {Component} from 'react';
-import './styles.css';
+import React, { Component } from "react";
+import "./styles.css";
 
 class Timer extends Component {
-	
-		state = {
-			timing: this.props.timer.autostart,
-			time: this.props.timer.time,
-			bar: 100			
-		};
-	
-		componentDidMount = () => {
-						this.timerID = setInterval(() => this.tick(), this.props.timer.timeTick); 
-		 }
+  state = {
+    timing: this.props.timer.autostart,
+    time: this.props.timer.time,
+    bar: 100,
+  };
 
-		tick = () => {
-			const currentTime = this.state.time;
-			const currentBar = this.state.bar;
-			const barTick = 100/(this.props.timer.time/this.props.timer.timeTick); 
-			if(this.state.timing === true){
-				if(currentTime > 0){
-					this.setState({time: currentTime - this.props.timer.timeTick});
-					this.setState({bar: currentBar - barTick})
-				} else {
-					this.setState({
-						timing: false, 
-						time: this.props.timer.time,
-						bar: 100
-					})
-					
-				}
-			}	
-		}
+  componentDidMount = () => {
+    this.timerID = setInterval(() => this.tick(), this.props.timer.timeTick);
+  };
 
+  tick = () => {
+    const barStep = 100 / (this.props.timer.time / this.props.timer.timeTick);
+    const currentTime = this.state.time;
+    const currentBar = this.state.bar;
+    if (this.state.timing === true) {
+      if (currentTime > 0) {
+        this.setState({ time: currentTime - this.props.timer.timeTick });
+        currentBar - barStep > 0
+          ? this.setState({ bar: currentBar - barStep })
+          : this.setState({ bar: 0 });
+      } else {
+        this.setState({
+          timing: false,
+          time: this.props.timer.time,
+          bar: 100,
+        });
+      }
+    }
+  };
 
-	  	 showButtonState= () => {
-	  	 	/*console.log(this.state.timing)*/
-	  	 	return this.state.timing === true ? "stop" : "start"
-	  	 } 
-	  	
-	 
+  showButtonTitle = () => {
+    return this.state.timing === true ? "stop" : "start";
+  };
 
-		createTimeString = (time) => {			
-			const checkTime = (i) => {return i<10 ? '0'+i : i};	  	
-			const ms = checkTime(time%1000);
-			const sec = checkTime(Math.floor(time/1000));
-			const min = checkTime(Math.floor(time/60000));
-			const str = min + ':' + sec + ':' + ms;
-			console.log(str);			
-			return str;
-		}		
+  createTimeString = (time) => {
+    let str = "";
+    if (time > 0) {
+      const checkTime = (i) => {
+        return i < 10 ? "0" + i : i;
+      };
+      const ms = checkTime(time % 1000);
+      const sec = checkTime(Math.floor(time / 1000));
+      const min = checkTime(Math.floor(time / 60000));
+      str = min + ":" + sec + ":" + ms;
+    } else {
+      str = "00:00:00";
+    }
+    console.log(str);    
+    return str;
+  };
 
-		handleClick = (e) =>{			
-			
-			this.state.timing ? this.setState({timing: false}) : this.setState({timing: true})
-		}
+  handleClick = (e) => {
+    this.state.timing
+      ? this.setState({ timing: false })
+      : this.setState({ timing: true });
+  };
 
-		render(){
-			const style = {width: this.state.bar+"%"}
-			return (
-				<div className= 'timer'>
-					<div className='timerBlock'>
-						<p>{this.createTimeString(this.state.time)}</p>
-					</div>
-					<button className= 'startButton' onClick={this.handleClick}>{this.showButtonState()}</button>
-					<div className='progressBar' style={style}></div>
-				</div>
-				)
-		}
-	
-
+  render() {
+    const style = { width: this.state.bar + "%" };
+    return (
+      <div className="timer">
+        <div className="timerBlock">
+          <p>{this.createTimeString(this.state.time)}</p>
+        </div>
+        <button className="startButton" onClick={this.handleClick}>
+          {this.showButtonTitle()}
+        </button>
+        <div className="progressBar" style={style}></div>
+      </div>
+    );
+  }
 }
 
 export default Timer;
-
-//<div className="bar" style={style}></div>
